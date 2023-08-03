3e8c32266da4d32a3580aff667eed9cd6cf3c7c2
Derek Meduri <derekmeduri@gmail.com>
2023-08-03 15:50:55 -0400

    adding foreignkey to indexjs

--- models/index.js
+++ models/index.js
@@ -16,9 +16,9 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "product_id" });
 
 module.exports = {
   Product,
